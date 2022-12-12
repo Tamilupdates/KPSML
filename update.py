8c3337bde153702547e551431253dd44293e0134
Tamil Updates <116391685+Tamilupdates@users.noreply.github.com>
2022-12-12 10:22:42 +0530

    Update update.py

--- update.py
+++ update.py
@@ -36,7 +36,7 @@ try:
     if len(UPSTREAM_REPO) == 0:
        raise TypeError
 except:
-    UPSTREAM_REPO = "https://github.com/Tamilupdates/Weebzone"
+    UPSTREAM_REPO = "https://github.com/Tamilupdates/KPSML"
 try:
     if len(UPSTREAM_BRANCH) == 0:
        raise TypeError
